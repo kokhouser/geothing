399aff4878907e56b51595d7edfcbaff0811b296
Jeffrey Killeen <jtk12b@acu.edu>
2016-05-15 09:43:34 -0500

    Added authentication with web tokens

--- api/_config.js
+++ api/_config.js
@@ -4,5 +4,6 @@ config.mongoURI = {
   development: 'mongodb://localhost/geothing',
   test: 'mongodb://localhost/geothingtest'
 };
+config.secret = 'thiswillbechangedinproduction';
 
 module.exports = config;
