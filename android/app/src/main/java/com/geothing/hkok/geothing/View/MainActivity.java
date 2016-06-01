026b1885709c74e511d304940a4988b41804bd19
Kok, HaoZhe <hkok@cvent.com>
2016-06-01 14:25:17 -0500

    Got basic Retrofit working + tests.

--- android/app/src/main/java/com/geothing/hkok/geothing/View/MainActivity.java
+++ android/app/src/main/java/com/geothing/hkok/geothing/View/MainActivity.java
@@ -1,7 +1,9 @@
-package com.geothing.hkok.geothing;
+package com.geothing.hkok.geothing.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.geothing.hkok.geothing.R;
 
 public class MainActivity extends AppCompatActivity {
 
