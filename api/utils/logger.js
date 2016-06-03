a9d903f516e5c203a7dac28787c4bb29a3ec954c
Kok, HaoZhe <hkok@cvent.com>
2016-06-03 14:11:19 -0500

    Fixed test logging.

--- api/utils/logger.js
+++ api/utils/logger.js
@@ -3,21 +3,25 @@ winston.emitErrs = true;
 
 var logger = new winston.Logger({
     transports: [
-        new winston.transports.File({
-            level: 'info',
-            filename: './logs/all-logs.log',
-            handleExceptions: true,
-            json: true,
-            maxsize: 5242880, //5MB
-            maxFiles: 5,
-            colorize: false
-        }),
         new winston.transports.Console({
+            name: 'console-info',
+            level: 'info',
+            //filename: './logs/all-logs.log',
+            //handleExceptions: true,
+            json: false,
+            //maxsize: 5242880, //5MB
+            //maxFiles: 5,
+            colorize: true
+        }),
+        /*
+        new winston.transports.Console({
+            name: 'console-debug',
             level: 'debug',
             handleExceptions: true,
             json: false,
             colorize: true
         })
+        */
     ],
     exitOnError: false
 });
